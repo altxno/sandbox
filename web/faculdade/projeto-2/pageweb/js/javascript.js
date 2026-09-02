4e8b185bebb227cf2f1c1ca74c4e302b9953a173
sunnycidal <manow.lucky@gmail.com>
2026-09-02 11:19:30 -0300

    aula-02-09-26

--- web/faculdade/projeto-2/pageweb/js/javascript.js
+++ web/faculdade/projeto-2/pageweb/js/javascript.js
@@ -1,4 +1,75 @@
-let salario = parseFloat(prompt("Digite o valor do salário:"));
-salario = salario * 1.25;
-document.write(salario)
-console.log(salario);
+// let salario = parseFloat(prompt("Digite o valor do salário:"));
+// salario = salario * 1.25;
+// document.write(salario)
+// console.log(salario);
+
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+// function sub(n1,n2){
+//     return n1 - n1;
+// }
+// function div(n1,n2){
+//     return n1 / n1;
+// }
+// function multi(n1,n2){
+//     return n1 * n1;
+// }
+//
+// let n1 = parseInt(prompt("Digite o primeiro número"));
+// let n2 = parseInt(prompt("Digite o segundo número"));
+// let opcao = parseInt(prompt("Digite a opção desejada \n 1. Soma \n 2. Divisão \n 3. Subtração \n 4. Multiplicação"));
+//
+// switch (opcao){
+//     case 1: document.write(soma(n1,n2));
+//         break;
+//     case 2: document.write(div(n1,n2));
+//         break;
+//     case 3: document.write(sub(n1,n2));
+//         break;
+//     case 4: document.write(multi(n1,n2));
+// }
+//
+//
+//
+// quadrado = () => {return (Math.pow(2,2))}
+// console.log(quadrado());
+
+
+// const readline = require('readline');
+//
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+//
+// filaMercado = ["altino", "guilerme"]
+// console.log(filaMercado)
+// rl.question('Próximo da fila:', (resposta) => {
+//     filaMercado.push(resposta)
+//     console.log(filaMercado)
+//     filaMercado.shift()
+//     console.log(filaMercado)
+//     filaMercado.pop()
+//     console.log(filaMercado)
+//
+//     rl.close();
+// });
+
+marcas = ['fiat','volks','ford','chevy']
+for (marca of marcas) {
+    document.write(marca+"<br>")
+}
+
+
+
+
+
+
+
+
+
+
+
+
