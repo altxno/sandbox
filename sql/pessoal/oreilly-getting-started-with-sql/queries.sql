82c1a44716f2891433534c201c5a53474c258820
Altino <manow.lucky@gmail.com>
2026-09-13 13:25:02 -0300

    termina capitulos 1 ao 4 (setup ambiente + select)

---
+++ sql/pessoal/oreilly-getting-started-with-sql/queries.sql
@@ -0,0 +1,23 @@
+-- Devido a arquitetura monobanco do SQLite, não é necessário "use <nomedobanco>"
+
+select * from customer;
+select customer_id, name from customer;
+select * from product;
+
+select
+    product_id,
+    description,
+    price as untaxed_price,
+    price * 1.07 as taxed_price
+from product;
+
+-- round(valor, casasdecimais)
+select *, round(price * 1.10, 2) as taxed_price from product;
+
+-- o operador "||" é de concatenação
+select
+    name,
+    city || ', ' || state as ADDRESS,
+    zip
+from customer;
+
